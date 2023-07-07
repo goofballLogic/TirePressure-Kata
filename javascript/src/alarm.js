830e04a009279daff142dd363429963b6678b525
Andrew Stewart Gibson <andrew@goofballLogic.com>
2023-07-07 16:49:26 +0100

    implemented unit test

--- javascript/src/alarm.js
+++ javascript/src/alarm.js
@@ -1,13 +1,12 @@
-import { sensor } from "./sensor.js";
+export const LOW_PRESSURE_THRESHOLD = 17;
+export const HIGH_PRESSURE_THRESHOLD = 21;
 
-const LOW_PRESSURE_THRESHOLD = 17;
-const HIGH_PRESSURE_THRESHOLD = 21;
-
-export function alarm() {
+export function alarm(sensor) {
 
     let count = 0;
     return () => {
 
+        console.log(count, sensor);
         if(count++ == 0) return false;
         const sensed = sensor();
         return (sensed < LOW_PRESSURE_THRESHOLD) || (sensed > HIGH_PRESSURE_THRESHOLD);
--- javascript/src/alarm.test.js
+++ javascript/src/alarm.test.js
@@ -1,18 +1,53 @@
-import { alarm } from "./alarm.js";
+import { HIGH_PRESSURE_THRESHOLD, LOW_PRESSURE_THRESHOLD, alarm } from "./alarm.js";
+import { sensor as actualSensor } from "./sensor.js";
 import assert from "node:assert/strict";
 
 describe("Alarm", () => {
 
-    let check;
+    let check, sensorSeam, defaultState;
     beforeEach(() => {
 
-        check = alarm();
+        sensorSeam = actualSensor;
+        check = alarm(() => sensorSeam());
+        defaultState = check();
 
     });
 
     test("Alarm is off by default", () => {
 
-        assert.equal(false, check());
+        assert.equal(false, defaultState);
+
+    });
+
+    describe("When sensor falls below threshold", () => {
+
+        beforeEach(() => {
+
+            sensorSeam = () => LOW_PRESSURE_THRESHOLD - 1;
+
+        });
+
+        test("Alarm is on", () => {
+
+            assert.equal(true, check());
+
+        });
+
+    });
+
+    describe("When the sensor rises above threshold", () => {
+
+        beforeEach(() => {
+
+            sensorSeam = () => HIGH_PRESSURE_THRESHOLD + 1;
+
+        });
+
+        test("Alarm is on", () => {
+
+            assert.equal(true, check());
+
+        });
 
     });
 
